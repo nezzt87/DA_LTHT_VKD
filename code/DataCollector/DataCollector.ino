cc5625e419df517305651c3137ad56f70eb98cfc
Lê Văn Cần <42709796+nezzt87@users.noreply.github.com>
2020-03-27 21:33:25 +0700

    Add comment to DataCollector, day la file chu yeu de phat trien sau nay, add JSON

--- code/DataCollector/DataCollector.ino
+++ code/DataCollector/DataCollector.ino
@@ -6,9 +6,22 @@
 #include <ArduinoJson.h>
 #include <Servo.h>
 #include <time.h>
+/*
+   BH1750
+   SCL <--> D3
+   SDA <--> D4
 
+   DHT11
+   DHTPIN <--> D5
 
-// Set these to run example.
+   Servo
+
+   attach <--> D2 (GPIO4)
+
+   con lai thi 3.3V voi GND
+*/
+
+// Set these to define some constant.
 #define FIREBASE_HOST "da-ltht-vdk-12.firebaseio.com"
 #define FIREBASE_AUTH "token_or_secret"
 #define WIFI_SSID "Quynh Anh"
@@ -19,17 +32,18 @@
 
 DHT dht(DHTPIN, DHTTYPE);
 BH1750 lightMeter(0x23);
+
 StaticJsonBuffer<256> jb;
 JsonObject& obj = jb.createObject();
+
 Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
 
 int dst = 0;
 
 void setup() {
   Serial.begin(9600);
   Serial.setDebugOutput(true);
-  
+
   dht.begin();
   // Initialize the I2C bus (BH1750 library doesn't do this automatically)
   Wire.begin(D4, D3);
@@ -50,8 +64,9 @@ void setup() {
   Serial.println();
   Serial.print("connected: ");
   Serial.println(WiFi.localIP());
-
+  //connect to Firebase
   Firebase.begin(FIREBASE_HOST);
+  //Test servo
   myservo.attach(4);  // attaches the servo on GIO4 to the servo object ~ D4
   int pos;
 
@@ -64,7 +79,9 @@ void setup() {
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
- configTime(TIMEZONE * 3600, 0, "pool.ntp.org", "time.nist.gov");
+
+  // config time
+  configTime(TIMEZONE * 3600, 0, "pool.ntp.org", "time.nist.gov");
   Serial.println("\nWaiting for time");
   while (!time(nullptr)) {
     Serial.print(".");
@@ -72,10 +89,11 @@ void setup() {
   }
   Serial.println("");
 }
-
+//bien chua thoi gian dang ddmmyy/HHMM
 char buffer[80];
-void loop() {
 
+void loop() {
+  
   float lux = lightMeter.readLightLevel();
   // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
@@ -84,23 +102,31 @@ void loop() {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
+  
   obj["Nhietdo"] = t;
-  
   obj["DoSang"] = lux;
-  
+  /*
+   * Get date and time
+   * Example /code/GetTime/GetTime.ino
+   * 
+   * strftime example
+   * http://www.cplusplus.com/reference/ctime/strftime/
+   */
   struct tm * timeinfo;
   time_t now = time(nullptr);
   Serial.println(ctime(&now));
   time (&now);
   timeinfo = localtime (&now);
-  strftime (buffer,80,"%d%m%y/%H%M%S",timeinfo);
-  
-  
+  strftime (buffer, 80, "%d%m%y/%H%M", timeinfo);
+
+  // Gui du lieu len firebase
   Firebase.set(String("/log/" + String(buffer)), obj);
   if (Firebase.failed()) {
     Serial.print("setting /number failed:");
     Serial.println(Firebase.error());
     return;
   }
-  delay(10000);
+
+  //Cach 1 phut thi thu thap du lieu 1 lan
+  delay(60000);
 }
